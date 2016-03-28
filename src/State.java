1fd4f6244a79e10b66501f7a2185202a9de014b8
DangB <bdang1991@gmail.com>
2016-03-27 21:46:41 -0400

    Final

---
+++ src/State.java
@@ -0,0 +1,44 @@
+public class State {
+	public int number;
+	public Boolean isAccepted = false;
+	public int ZeroTransition;
+	public int OneTransition;
+
+	public State (int number) {
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setZeroTransition(int n) {
+		ZeroTransition = n;
+	}
+
+	public void setOneTransition(int n) {
+		OneTransition = n;
+	}
+        
+        public void makeAccepting() {
+            isAccepted = true;
+        }
+
+	public int getZeroTransition() {
+		return ZeroTransition;
+	}
+
+	public int getOneTransition() {
+		return OneTransition;
+	}
+        
+        public void print() {
+            System.out.println("------------------------");
+            System.out.println("State Number: " + number +
+                    "\nZero Transition: " + ZeroTransition +
+                    "\nOne Transition: " + OneTransition +
+                    "\nAccepted State: " + isAccepted);
+            System.out.println("------------------------");
+        }
+
+}
